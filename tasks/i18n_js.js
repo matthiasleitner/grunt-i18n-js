a062533f823b1faffdfbbbe53179a58d3757e894
Matthias Leitner <matthias.leitner@runtastic.com>
2015-01-07 09:26:13 +0100

    improve configuration

--- tasks/i18n_js.js
+++ tasks/i18n_js.js
@@ -7,7 +7,7 @@
  */
 
 'use strict';
-  
+
 module.exports = function(grunt) {
 
   var _    = require('lodash');
@@ -15,11 +15,15 @@ module.exports = function(grunt) {
   var done = null;
   var translations = {};
   var options = {};
-  var I18N_AVAILABLE_LOCALES = ["en","de","es","it","nl","fr","cs","ja","pl","pt-br","pt-pt","ru","zh","tr"];
+  var I18N_AVAILABLE_LOCALES = ['en','de','es','it','nl','fr','cs','ja','pl','pt-br','pt-pt','ru','zh','tr'];
 
-  grunt.registerMultiTask('i18n', 'Convert rails locales to i18n-js compatible js files', function() {    
+  grunt.registerMultiTask('i18n', 'Convert rails locales to i18n-js compatible js files', function() {
     done = this.async();
     options = this.options({});
+
+    if (!options.railsPath) {
+      grunt.fatal('You need to provide the railsPath option!');
+    };
 
     localePaths.call(this);
   });
@@ -37,15 +41,15 @@ module.exports = function(grunt) {
   }
 
   function save(translationData, filepath){
-    filepath = filepath.replace("app/assets/javascripts/", "");
+    filepath = filepath.replace('app/assets/javascripts/', '');
 
-    var contents = "var I18n = I18n || {}; \n";
-    contents += "I18n.translations = I18n.translations || {}; \n";
-    contents += "jQuery.extend(true, I18n.translations, ";
+    var contents = 'var I18n = I18n || {}; \n';
+    contents += 'I18n.translations = I18n.translations || {}; \n';
+    contents += 'jQuery.extend(true, I18n.translations, ';
     contents += JSON.stringify(translationData);
-    contents += ");";
+    contents += ');';
 
-    grunt.log.writeln("Saving to file " + filepath);
+    grunt.log.writeln('Saving to file ' + filepath);
     grunt.file.write(filepath, contents);
   }
 
@@ -58,12 +62,12 @@ module.exports = function(grunt) {
   }
 
   function validConfigPresent(){
-    return (configExists() && config()["translations"]);
+    return (configExists() && config()['translations']);
   }
 
   function singleFileExport(){
     var segments = {};
-    segments[exportDir() + "/translations.js"] = translations;
+    segments[exportDir() + '/translations.js'] = translations;
     return segments;
   }
 
@@ -71,17 +75,17 @@ module.exports = function(grunt) {
     var fallbackLoc    = fallbackLocale(fallback);
     var fallbackScope  = scopeWithLocale(scope, fallbackLoc);
     var fallbackResult = scopedTranslations(fallbackScope)
-   
+
     if(!result[locale]) {
       result[locale] = {};
     }
-    
+
     result[locale] = _.merge(fallbackResult[fallbackLoc],result[locale]);
   }
 
   function scopeWithLocale(scope, locale){
     var flatScope = _.flatten([scope]);
-    return _.map(flatScope, function(s) { return locale + "." + s; });
+    return _.map(flatScope, function(s) { return locale + '.' + s; });
   }
 
   function scopedTranslations(scopes) {
@@ -111,9 +115,9 @@ module.exports = function(grunt) {
 
   function filter(translationData, scopes) {
     if (typeof scopes == 'string' || scopes instanceof String) {
-      scopes = scopes.split(".")
+      scopes = scopes.split('.')
     }
-      
+
     scopes = clone(scopes);
     var scope = scopes.shift();
 
@@ -135,18 +139,18 @@ module.exports = function(grunt) {
   }
 
   function fallbackLocale(locale){
-    return I18N_AVAILABLE_LOCALES.indexOf(locale) > 0 ? locale : "en";
+    return allLocales().indexOf(locale) > 0 ? locale : 'en';
   }
 
   function segmentsPerLocale(pattern, scope, fallback){
     var segments = {};
 
-    I18N_AVAILABLE_LOCALES.forEach(function(locale){
+    allLocales().forEach(function(locale){
       var localeScopes = scopeWithLocale(scope, locale);
       var result = scopedTranslations(localeScopes);
 
       if(fallback) {
-        mergeWithFallback(result, locale, scope, fallback);  
+        mergeWithFallback(result, locale, scope, fallback);
       }
 
       if(!_.isEmpty(result)) {
@@ -158,29 +162,33 @@ module.exports = function(grunt) {
     return segments;
   }
 
+  function allLocales(){
+    return options.locales || I18N_AVAILABLE_LOCALES;
+  }
+
   function configExists(){
-    return grunt.file.exists(configFile());
+    return grunt.file.exists(configFileLocation());
   }
 
   function configuredSegments(){
     var segments = {};
 
-    config()["translations"].forEach(function(options){
-      _.merge({"only":"*", "fallback": false}, options);
+    config()['translations'].forEach(function(options){
+      _.merge({'only':'*', 'fallback': false}, options);
 
-      if (_.include(options["file"], '%{locale}')) {
-        var segmentsToMerge = segmentsPerLocale(options["file"], 
-                                                options["only"], 
-                                                options["fallback"]);
-      
+      if (_.include(options['file'], '%{locale}')) {
+        var segmentsToMerge = segmentsPerLocale(options['file'],
+                                                options['only'],
+                                                options['fallback']);
+
         _.assign(segments, segmentsToMerge);
       } else {
-        var result = segmentForScope(options["only"]);
-        var segmentKey = options["file"];
+        var result = segmentForScope(options['only']);
+        var segmentKey = options['file'];
 
         if(!_.isEmpty(result)){
-          segments[segmentKey] = result;  
-        }        
+          segments[segmentKey] = result;
+        }
       }
     });
 
@@ -193,8 +201,8 @@ module.exports = function(grunt) {
 
     walker.on('file', function(root, stat, next) {
       if(_.contains(stat.name, '.yml')){
-        files.push(root + '/' + stat.name);  
-      }  
+        files.push(root + '/' + stat.name);
+      }
       next();
     });
 
@@ -205,7 +213,8 @@ module.exports = function(grunt) {
   }
 
   function localePath(){
-    return options.railsPath + "/app/locales";
+    var relativeLocalePath = options.localePath || '/app/locales';
+    return options.railsPath + relativeLocalePath;
   }
 
   function segmentForScope(scope){
@@ -216,8 +225,9 @@ module.exports = function(grunt) {
     };
   }
 
-  function configFile(){
-    return options.railsPath + "/config/i18n-js.yml";
+  function configFileLocation(){
+    var configFileName = options.configFile || '/config/i18n-js.yml';
+    return options.railsPath + configFileName;
   }
 
   function readLocaleFiles(paths){
@@ -231,7 +241,7 @@ module.exports = function(grunt) {
   }
 
   function config(){
-    return grunt.file.readYAML(configFile());
+    return grunt.file.readYAML(configFileLocation());
   }
 
   function exportDir(){
